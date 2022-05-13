903475db9ef7ce42de7dccfccc811173ce385e28
Lulupointu <lucasdelsol@hotmail.fr>
2022-05-13 17:31:55 +0200

    rebuild with nice favicon

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "91f1be4d60cfb8cef6deae7ba970bfcd",
-"index.html": "96bf4d803ad20a2467cb72c28798f47f",
-"/": "96bf4d803ad20a2467cb72c28798f47f",
+"index.html": "9e4677179265df9db687726d5c587e0f",
+"/": "9e4677179265df9db687726d5c587e0f",
 "main.dart.js": "6555a55122374a09ac1a0ae8708365ef",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "7ccc5cf69998efd991c4982dfb22db17",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
